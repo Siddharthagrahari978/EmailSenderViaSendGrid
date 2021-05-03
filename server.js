d729d2f72f85be9a0d8c93b24137aa84fb9a28b4
Siddharthagrahari978 <siddharthagrahari978@gmail.com>
2021-05-03 18:28:46 +0530

    some tweaks

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 
-const moment = require('moment')
 //? Send Grid connection
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
@@ -12,19 +11,6 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 app.use(express.static('./public'));
-
-
-function formatDateTimeForUser (date) {
-  var userTZ,     // user specified TZ like Europe/Berlin
-      userTZoffset, // tzOffset we got from login form
-      userDateFormat,  // user specified date format (or default)
-      userTimeFormat; // user specified time format (or default)
-
-  if (userTZ)
-      return moment(date).tz(userTZ).format(userDateFormat+' '+userTimeFormat+' zz');
-  else
-      return moment(date).utcOffset(userTZoffset).format(userDateFormat+' '+userTimeFormat+' ZZ');
-}
 
 
 //! Deploying app
@@ -40,11 +26,9 @@ server.listen(PORT, () =>{
       sgMail
       .send(msg)
       .then(() => {
-        console.log(`Email sent at ${moment().format('h:mm a')}`)
-        socket.emit('sent', `Email sent at ${moment().format('h:mm a')}`)
+        socket.emit('sent', `Email sent`)
       })
       .catch((error)=> {
-        console.log(error)
         socket.emit('error', error)
       }) 
     })
