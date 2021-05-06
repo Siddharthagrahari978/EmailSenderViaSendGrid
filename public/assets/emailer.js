24a5d61610859bb03bdfc3dc78563e55f26311e5
Siddharthagrahari978 <siddharthagrahari978@gmail.com>
2021-05-06 16:02:20 +0530

    Major Tweaks

--- public/assets/emailer.js
+++ public/assets/emailer.js
@@ -3,6 +3,7 @@ const socket = io();
 
 const mailForm = document.querySelector("#mailForm")
 
+const formContainer = document.querySelector('.formContainer');
 
 let responce = document.querySelector('#responce')
 
@@ -15,10 +16,21 @@ mailForm.addEventListener('submit', (event) => {
   socket.emit('sendMail', to, subject, text);
 })
 socket.on('sent', msg =>{
-  responce.innerHTML = `<div class='alert alert-success' role='alert' data-mdb-color='success' >${msg}</div>`
+  formContainer.style.display = 'none';
+  responce.innerHTML = `<div class='alert alert-success' role='alert' data-mdb-color='success' >${msg}</div>
+  <button id='sendAnother' onClick='sendAnother()' class="btn btn-success btn-block mb-4">Send Another</button>`
 })
 socket.on('error',error =>{
   responce.innerHTML = `<div class='alert alert-danger' role='alert' data-mdb-color='danger' >Sorry we couldn't send your email</div>`
   alert('Failed to send email')
   console.log(error)
 })
+
+function sendAnother() {
+  formContainer.style.display = 'block'
+  responce.innerHTML = ''
+  document.querySelector('#rescipient_email').value = null
+  document.querySelector('#subject').value = null
+  document.querySelector('#message').value = null
+
+}
